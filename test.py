448761264e074587e643a87c82a2764202d0be44
Sam Holt <samuel.holt.direct@gmail.com>
2023-03-28 02:18:10 +0100

    update

--- test.py
+++ test.py
@@ -2,20 +2,21 @@ import gym
 
 from stable_baselines3 import DQN
 
-env = gym.make("CartPole-v1")
+# env = gym.make("CartPole-v1")
+env = gym.make("FrozenLake-v1")
 
 model = DQN("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=500_000)
+model.learn(total_timesteps=100_000)
 
 vec_env = model.get_env()
 obs = vec_env.reset()
 for i in range(1000):
     action, _states = model.predict(obs, deterministic=True)
-    obs, reward, done, truncated, info = vec_env.step(action)
+    obs, reward, done, info = vec_env.step(action)
     vec_env.render()
     # VecEnv resets automatically
     if done:
       break
 
 env.close()
-print(f'Episode reward: {info["episode"]["r"]}')
+print(f'Episode reward: {info[0]["episode"]["r"]}')
